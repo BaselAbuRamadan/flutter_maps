12dea2631c71558b298a929c40fe782082b7c95f
Eng.Basel <besooking22@gmail.com>
2022-08-21 10:02:44 +0300

    please insert your own private google maps api key to use the map

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,5 +1,5 @@
 const loginScreen = '/login-screen';
 const otpScreen = '/otp-screen';
 const mapScreen = '/map-screen';
-const googleAPIKey = 'AIzaSyDsabuPLvzMsrFwuMIHRvXscGTRV-2QQVk';
+const googleAPIKey = 'Put Your Api here';
 const suggestionBaseUrl = 'https://maps.googleapis.com/maps/api/place/autocomplete/json';
